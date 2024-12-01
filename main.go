8921dc0d7a1e5573c2677f18fc2a18a9106b788e
radovanrasha <kking1755@gmail.com>
2024-12-01 09:09:38 +0100

    Functionality for deployment of portfolio project

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -23,6 +26,53 @@ func main() {
 	w.ShowAndRun()
 }
 
+func executeSSHCommand(user, host, commands string) (string, error) {
+	var output bytes.Buffer
+	cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", user, host), commands)
+
+	cmd.Stdout = &output
+	cmd.Stderr = &output
+
+	err := cmd.Run()
+	return output.String(), err
+}
+
+func deployPortfolio() {
+	err := godotenv.Load()
+    if err != nil {
+        fmt.Println("Error loading .env file")
+        return
+    }
+	// VPS connection details
+	vpsUser := os.Getenv("VPS_USER")
+    vpsHost := os.Getenv("VPS_HOST")
+    projectPath := os.Getenv("PROJECT_PATH")
+
+	commands := fmt.Sprintf(`
+		export LC_ALL=C.UTF-8
+		export LANG=C.UTF-8
+		export NVM_DIR="$HOME/.nvm"
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Load nvm
+		cd %s && \
+		git pull && \
+		nvm install 20.17.0 && \ # Ensure Node.js 20 is installed
+		nvm use 20.17.0 && \ # Use Node.js 20
+		npm install && \
+		npm run build && \
+		pm2 restart portfolio
+	`, projectPath)
+
+	// Execute the SSH command
+	output, err := executeSSHCommand(vpsUser, vpsHost, commands)
+	if err != nil {
+		fmt.Println("Output:", output)
+		fmt.Printf("Deployment failed: %s\n", err.Error())
+	} else {
+		fmt.Println("Output:", output)
+		fmt.Println("Deployment succeeded!")
+	}
+}
+
 func SetupUI(w fyne.Window) {
 	// Set window content
 	w.SetContent(
@@ -34,7 +84,7 @@ func SetupUI(w fyne.Window) {
 				container.NewBorder(
 					nil,nil,
 					widget.NewButton("Deploy!", func() {
-						w.Close()
+						deployPortfolio()
 					}),
 					nil,
 				),
